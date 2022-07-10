e075b1c5d9e4dafd45d556f42f27545b0b22b110
speedlight <tao.ceel@gmail.com>
2022-07-10 14:41:50 -0500

    adding variables to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,9 +18,9 @@ resource "google_storage_bucket" "bucket" {
   location  =  var.region
 }
 
-data "archive_file" "enqueuer" {
+data "archive_file" "left_function_zip" {
   type        = "zip"
-  output_path = "${path.module}/files/enqueuer.zip"
+  output_path = "${path.module}/files/left_function.zip"
   source {
     content  = "${file("../enqueuer.js")}"
     filename = "index.js"
@@ -31,9 +31,9 @@ data "archive_file" "enqueuer" {
   }
 }
 
-data "archive_file" "logger" {
+data "archive_file" "right_function_zip" {
   type        = "zip"
-  output_path = "${path.module}/files/logger.zip"
+  output_path = "${path.module}/files/right_function.zip"
   source {
     content  = "${file("../logger.js")}"
     filename = "index.js"
@@ -44,63 +44,63 @@ data "archive_file" "logger" {
   }
 }
 
-resource "google_storage_bucket_object" "enqueuer" {
+resource "google_storage_bucket_object" "left_function_object" {
   name   = "enqueuer.zip"
   bucket = "${google_storage_bucket.bucket.name}"
   source = "${path.module}/files/enqueuer.zip"
-  depends_on = [data.archive_file.enqueuer]
+  depends_on = [data.archive_file.left_function_zip]
 }
 
-resource "google_storage_bucket_object" "logger" {
+resource "google_storage_bucket_object" "right_function_object" {
   name   = "logger.zip"
   bucket = "${google_storage_bucket.bucket.name}"
   source = "${path.module}/files/logger.zip"
-  depends_on = [data.archive_file.logger]
+  depends_on = [data.archive_file.right_function_zip]
 }
 
-resource "google_cloudfunctions_function" "enqueuer" {
-  name        = "enqueuer"
+resource "google_cloudfunctions_function" "left_function" {
+  name        = var.left_function_name
   description = "Enqueuer"
   runtime     = var.functions_runtime
   available_memory_mb   = 128
   source_archive_bucket = google_storage_bucket.bucket.name
-  source_archive_object = google_storage_bucket_object.enqueuer.name
+  source_archive_object = google_storage_bucket_object.left_function_object.name
   trigger_http          = true
   timeout               = 60
   entry_point           = "mainFunction"
 }
 
-resource "google_cloudfunctions_function" "logger" {
-  name        = "logger"
+resource "google_cloudfunctions_function" "right_function" {
+  name        = var.right_function_name
   description = "Logger"
   runtime     = var.functions_runtime
   available_memory_mb   = 128
   source_archive_bucket = google_storage_bucket.bucket.name
-  source_archive_object = google_storage_bucket_object.logger.name
+  source_archive_object = google_storage_bucket_object.right_function_object.name
   trigger_http          = true
   timeout               = 60
   entry_point           = "mainFunction"
 }
 
-resource "google_cloudfunctions_function_iam_member" "enqueuer-invoker" {
-  project        = google_cloudfunctions_function.enqueuer.project
-  region         = google_cloudfunctions_function.enqueuer.region
-  cloud_function = google_cloudfunctions_function.enqueuer.name
+resource "google_cloudfunctions_function_iam_member" "left-function-invoker" {
+  project        = google_cloudfunctions_function.left_function.project
+  region         = google_cloudfunctions_function.left_function.region
+  cloud_function = google_cloudfunctions_function.left_function.name
   role   = "roles/cloudfunctions.invoker"
   member = "allUsers"
 }
 
-resource "google_cloudfunctions_function_iam_member" "logger-invoker" {
-  project        = google_cloudfunctions_function.logger.project
-  region         = google_cloudfunctions_function.logger.region
-  cloud_function = google_cloudfunctions_function.logger.name
+resource "google_cloudfunctions_function_iam_member" "right-function-invoker" {
+  project        = google_cloudfunctions_function.right_function.project
+  region         = google_cloudfunctions_function.right_function.region
+  cloud_function = google_cloudfunctions_function.right_function.name
   role   = "roles/cloudfunctions.invoker"
   member = "allUsers"
 }
 
 /*
-resource "google_cloud_tasks_queue" "enqueuer-logger" {
-  name     = "enqueuer-logger"
+resource "google_cloud_tasks_queue" "functions_queue" {
+  name     = var.queue_name
   location = var.region
   rate_limits {
     max_concurrent_dispatches = 100
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,34 @@
 variable "project_id" {
-  type = "string"
+  type = string
   default = "ringed-enigma-334420"
 }
 
 variable "region" {
-  type = "string"
+  type = string
   default = "northamerica-northeast1"
 }
 
 variable "bucket_name" {
-  type = "string"
+  type = string
   default = "task-queue"
 }
 
 variable "functions_runtime" {
-  type = "string"
+  type = string
   default = "nodejs16"
 }
 
+variable "queue_name" {
+  type = string
+  default = "enqueuer-logger"
+}
+
+variable "left_function_name" {
+  type = string
+  default = "enqueuer"
+}
+
+variable "right_function_name" {
+  type = string
+  default = "enqueuer"
+}
